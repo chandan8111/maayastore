a59cffd125dec2dc3e84a684907f1e868d111aa0
Maya Group <bestangleon@gmail.com>
2020-10-09 21:20:21 +0530

    add templates into both app

--- shop/views.py
+++ shop/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Home of Shop")
+    return render(request, 'shop/index.html')
