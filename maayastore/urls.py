155c317078a2ab565de3c592dd642c0a5819564a
Maya Group <bestangleon@gmail.com>
2020-10-11 10:18:09 +0530

    Add Static file or directory in blog app

--- maayastore/urls.py
+++ maayastore/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls'))
 ]
