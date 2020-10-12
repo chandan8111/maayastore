09b2962e51d52d61dc72ad544d72b6518064879c
Maya Group <bestangleon@gmail.com>
2020-10-12 20:48:34 +0530

    Adding images throw using models

--- shop/models.py
+++ shop/models.py
@@ -4,5 +4,13 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=200)
-    product_desc = models.CharField(max_length=50000)
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    product_desc = models.CharField(max_length=10000)
+    product_price = models.IntegerField(default=0)
+    product_image = models.ImageField(upload_to="shop/images", default="")
     publish_date = models.DateField()
+
+
+    def __str__(self):
+        return self.product_name
