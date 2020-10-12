c5e1073f2452239357b6551744f08a1cf31d04f6
Maya Group <bestangleon@gmail.com>
2020-10-12 15:13:57 +0530

    add E-commerce website url pattern in our shop app

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name="shophome")
+    path('', views.index, name="ShopHome"),
+    path('about/', views.about, name="AboutUs"),
+    path('contact/', views.contact, name="Contact"),
+    path('traker/', views.traker, name="Traker"),
+    path('search/', views.search, name="Search"),
+    path('productview/', views.productview, name="ProductView"),
+    path('checkout/', views.checkout, name="Chekout")
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("We Are at About")
+
+def contact(request):
+    return HttpResponse("We Are at Contact")
+
+def traker(request):
+    return HttpResponse("We Are at Traker")
+
+def search(request):
+    return HttpResponse("We Are at Search")
+
+def productview(request):
+    return HttpResponse("We Are at Product View")
+
+def checkout(request):
+    return HttpResponse("We Are at Checkout")
