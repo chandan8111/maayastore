d8f4cdda9c584bf28bb41975d34de45588d85aca
Maya Group <bestangleon@gmail.com>
2020-10-15 17:20:36 +0530

    Showing product on homepage thow the database data using

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    n = len(products)
+    number_slides = n // 3 + ceil((n/3)-(n//3))
+    params = {'product': products, 'no_of_slides': number_slides, 'range':range(1, number_slides)}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     return render(request, 'shop/about.html')
