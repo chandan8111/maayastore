0040aba032c13de2546cf2f1c7a4043b910103a3
Maya Group <bestangleon@gmail.com>
2020-11-17 21:06:03 +0530

    Nothing Change

--- shop/models.py
+++ shop/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=200)
     category = models.CharField(max_length=50, default="")
     subcategory = models.CharField(max_length=50, default="")
-    product_desc = models.CharField(max_length=10000)
+    product_desc = models.TextField(max_length=10000)
     product_price = models.IntegerField(default=0)
     product_image = models.ImageField(upload_to="shop/images", default="")
     publish_date = models.DateField()
@@ -20,7 +20,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50, default="")
     phone = models.CharField(max_length=20, default="")
-    desc = models.CharField(max_length=5000, default="")
+    desc = models.TextField(max_length=5000, default="")
 
     def __str__(self):
         return self.name
