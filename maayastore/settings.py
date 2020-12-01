63d9bafbea449e9073254d7a3ff5f59c0bbc2e8e
Maya Group <bestangleon@gmail.com>
2020-12-01 14:24:50 +0530

    Improvement in maayashop main (home) page

--- maayastore/settings.py
+++ maayastore/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'maayastore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['maayastore/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
