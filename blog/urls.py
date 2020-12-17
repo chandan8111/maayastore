04eb518ef970c3cba960fb24fb1308f2321fa2b5
Maya Group <bestangleon@gmail.com>
2020-12-17 13:23:46 +0530

    Creating blog app template

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name="bloghome")
+    path('', views.index, name="blogHome"),
+    path('blogpost/', views.blogpost, name="blogPost")
 ]
